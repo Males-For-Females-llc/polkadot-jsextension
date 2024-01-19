4ed613307a3f9f1c2c32ef0af4e1e728d24e13dd
Madeline Nicole Brown <104762490+Madib036@users.noreply.github.com>
2024-01-19 07:16:25 -0600

    Initialize jg-ledger-direct

---
+++ packages/extension-base/src/background/RequestBytesSign.ts
@@ -0,0 +1,28 @@
+// Copyright 2019-2021 @polkadot/extension authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { KeyringPair } from '@polkadot/keyring/types';
+import type { SignerPayloadRaw } from '@polkadot/types/types';
+import type { RequestSign } from './types';
+
+import { wrapBytes } from '@polkadot/extension-dapp/wrapBytes';
+import { TypeRegistry } from '@polkadot/types';
+import { u8aToHex } from '@polkadot/util';
+
+export default class RequestBytesSign implements RequestSign {
+  public readonly payload: SignerPayloadRaw;
+
+  constructor (payload: SignerPayloadRaw) {
+    this.payload = payload;
+  }
+
+  sign (_registry: TypeRegistry, pair: KeyringPair): { signature: string } {
+    return {
+      signature: u8aToHex(
+        pair.sign(
+          wrapBytes(this.payload.data)
+        )
+      )
+    };
+  }
+}
